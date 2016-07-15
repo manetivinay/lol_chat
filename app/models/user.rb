efed4450a5e156b21c01c974f38854f2371df43f
Vinay Maneti <vinaymaneti@gmail.com>
2016-07-16 00:52:23 +0700

    +Added User can sign up by providing their email, password, and name. :dance:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: {case_insenssiitve: false}
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  has_secure_password
 end
